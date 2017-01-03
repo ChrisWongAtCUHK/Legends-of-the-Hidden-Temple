37de4a518865c28047ee097263f2fdbb5bf50f3e
chriswongatcuhk <wc38.wong@gmail.com>
2017-01-03 22:59:50 +0800

    Remove unnecessary condition check.

--- src/EightiesKids4.java
+++ src/EightiesKids4.java
@@ -8,9 +8,7 @@ public class EightiesKids4 {
 		  int spaceCount2 = (int) (n * 2 - 3);
 		  // upper half
 		  for(int i = 0; i < n / 2 - 1; i++){
-			  if(spaceCount1 > 0){
-				  sb.append(new String(new char[spaceCount1]).replace("\0", " "));
-			  }
+			  sb.append(new String(new char[spaceCount1]).replace("\0", " "));
 			  sb.append("X");
 			  sb.append(new String(new char[spaceCount2]).replace("\0", " "));
 			  sb.append("X\n");
@@ -28,13 +26,9 @@ public class EightiesKids4 {
 		  
 		  // lower half
 		  for(int i = (int) (n / 2) + 1; i < n; i++){
-			  if(spaceCount1 > 0){
-				  sb.append(new String(new char[spaceCount1]).replace("\0", " "));
-			  }
+			  sb.append(new String(new char[spaceCount1]).replace("\0", " "));
 			  sb.append("X");
-			  if(spaceCount2 > 0){
-				  sb.append(new String(new char[spaceCount2]).replace("\0", " "));
-			  }
+			  sb.append(new String(new char[spaceCount2]).replace("\0", " "));
 			  sb.append("X\n");
 			  
 			  spaceCount1 -= 2;
