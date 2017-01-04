f05144978b28f0f483f111df2dec7eb7008bb329
chriswongatcuhk <wc38.wong@gmail.com>
2017-01-04 21:07:52 +0800

    Comment.

--- src/EightiesKids4.java
+++ src/EightiesKids4.java
@@ -4,21 +4,22 @@ public class EightiesKids4 {
 	  // your code here
 	  if(n % 2 == 1) {
 		  
-		  int leftSpaceCount = (int) (n - 1);
-		  int centerSpaceCount = 3;
+		  int leftSpaceCount = (int) (n - 1);		// magic number 1
+		  int centerSpaceCount = 3;					// magic number 2
 		  
+		  // center X, only one
 		  String result = String.format("%sX\n", new String(new char[leftSpaceCount]).replace("\0", " "));
 		  
-		  for(int i = 1; i <= (int)(n / 2); i++){
+		  for(int i = 1; i <= (int)(n / 2); i++){		// start from 1, end after looping half of n
 			  String line =  
-					  String.format("%sX%sX\n", 
-							  new String(new char[leftSpaceCount - 2]).replace("\0", " "),
-							  new String(new char[centerSpaceCount]).replace("\0", " ")
+					  String.format("%sX%sX\n", 		// 2 Xs with spaces
+							  new String(new char[leftSpaceCount - 2]).replace("\0", " "),	// left spaces before first X
+							  new String(new char[centerSpaceCount]).replace("\0", " ")		// center spaces between first and second X
 					  );
-			  result = String.format("%s%s%s", line, result, line);
+			  result = String.format("%s%s%s", line, result, line);		// vertical symmetry
 			  
-			  leftSpaceCount -= 2;
-			  centerSpaceCount += 4;
+			  leftSpaceCount -= 2;			// every line, decreases 2 spaces
+			  centerSpaceCount += 4;		// every line, increases 4 spaces
 		  }
 		  
 		  return result;
