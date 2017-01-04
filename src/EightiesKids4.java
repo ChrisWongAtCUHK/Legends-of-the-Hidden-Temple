bf29c87e8ff4823498b63028ca21de06c51f6664
chriswongatcuhk <wc38.wong@gmail.com>
2017-01-04 20:47:42 +0800

    Simplify the pattern.

--- src/EightiesKids4.java
+++ src/EightiesKids4.java
@@ -3,39 +3,25 @@ public class EightiesKids4 {
   public static String markSpot(float n) {
 	  // your code here
 	  if(n % 2 == 1) {
-		  StringBuilder sb = new StringBuilder();
-		  int spaceCount1 = 0;
-		  int spaceCount2 = (int) (n * 2 - 3);
-		  // upper half
-		  for(int i = 0; i < n / 2 - 1; i++){
-			  sb.append(new String(new char[spaceCount1]).replace("\0", " "));
-			  sb.append("X");
-			  sb.append(new String(new char[spaceCount2]).replace("\0", " "));
-			  sb.append("X\n");
+		  
+		  int leftSpaceCount = (int) (n - 1);
+		  int centerSpaceCount = 3;
+		  
+		  String result = String.format("%sX\n", new String(new char[leftSpaceCount]).replace("\0", " "));
+		  
+		  for(int i = 1; i <= (int)(n / 2); i++){
+			  String line =  
+					  String.format("%sX%sX\n", 
+							  new String(new char[leftSpaceCount - 2]).replace("\0", " "),
+							  new String(new char[centerSpaceCount]).replace("\0", " ")
+					  );
+			  result = String.format("%s%s%s", line, result, line);
 			  
-			  spaceCount1 += 2;
-			  spaceCount2 -= 4;
+			  leftSpaceCount -= 2;
+			  centerSpaceCount += 4;
 		  }
 		  
-		  // the center
-		  sb.append(new String(new char[(int) (n - 1)]).replace("\0", " "));
-		  sb.append("X\n");
-		  
-		  spaceCount1 -= 2;
-		  spaceCount2 += 4;
-		  
-		  // lower half
-		  for(int i = (int) (n / 2) + 1; i < n; i++){
-			  sb.append(new String(new char[spaceCount1]).replace("\0", " "));
-			  sb.append("X");
-			  sb.append(new String(new char[spaceCount2]).replace("\0", " "));
-			  sb.append("X\n");
-			  
-			  spaceCount1 -= 2;
-			  spaceCount2 += 4;
-		  }
-		  
-		  return sb.toString();
+		  return result;
 	  }
 	  return "?";
   }
